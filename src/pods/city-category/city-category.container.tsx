52c2b7e8e39e5845e2b54d35458da83575d310d5
Guste Gaubaite <219.guste@gmail.com>
2025-04-21 15:53:38 +0200

    Adapt landscape and city categories to use ImageCategory component

--- src/pods/city-category/city-category.container.tsx
+++ src/pods/city-category/city-category.container.tsx
@@ -1,57 +1,13 @@
-import { FC, useContext, useEffect } from "react";
-import { CityCategoryComponent } from "./city-category.component";
+import { FC } from "react";
 import { getPictures } from "./city-category.api";
-import { mapPictureCollectionFromApiToVm, PictureInfoVm } from "./city.vm";
-import { PicturesContext } from "../../core/context/pictures-context";
+import { mapPictureCollectionFromApiToVm } from "./city.vm";
+import { ImageCategoryContainer } from "../image-category";
 
 export const CityCategoryContainer: FC = () => {
-  const { pictures, setPictures, selectedPictures, setSelectedPictures } =
-    useContext(PicturesContext);
-
-  const handleCheckBox = (id: string) => {
-    const selection = pictures.find((picture) => picture.id === id);
-
-    if (selection) {
-      selection.selected = !selection.selected;
-      const updatedPictures = pictures.map((picture) =>
-        picture.id === selection.id
-          ? {
-              ...picture,
-              selected: selection?.selected,
-            }
-          : picture
-      );
-      setPictures(updatedPictures);
-      addDeleteFromCart(selection, id);
-    }
-  };
-
-  const addDeleteFromCart = (selection: PictureInfoVm, id: string) => {
-    const updateWithDeletedPicture = selectedPictures.filter(
-      (picture) => picture !== selection.id
-    );
-
-    if (selection.selected === true)
-      setSelectedPictures([...selectedPictures, id]);
-    else {
-      setSelectedPictures(updateWithDeletedPicture);
-    }
-  };
-
-  useEffect(() => {
-    getPictures().then((apiPictures) => {
-      const mappedPictures = mapPictureCollectionFromApiToVm(
-        apiPictures,
-        selectedPictures
-      );
-      setPictures(mappedPictures);
-    });
-  }, [selectedPictures]);
-
   return (
-    <CityCategoryComponent
-      pictures={pictures}
-      handleCheckBox={handleCheckBox}
+    <ImageCategoryContainer
+      getPictures={getPictures}
+      mapPictureCollectionFromApiToVm={mapPictureCollectionFromApiToVm}
     />
   );
 };
--- src/pods/landscape-category/landscape-category.container.tsx
+++ src/pods/landscape-category/landscape-category.container.tsx
@@ -1,57 +1,13 @@
-import { FC, useContext, useEffect } from "react";
-import { LandscapeCategoryComponent } from "./landscape-category.component";
+import { FC } from "react";
 import { getPictures } from "./landscape-category.api";
-import { mapPictureCollectionFromApiToVm, PictureInfoVm } from "./landscape.vm";
-import { PicturesContext } from "../../core/context/pictures-context";
+import { mapPictureCollectionFromApiToVm } from "./landscape.vm";
+import { ImageCategoryContainer } from "../image-category";
 
 export const LandscapeCategoryContainer: FC = () => {
-  const { pictures, setPictures, selectedPictures, setSelectedPictures } =
-    useContext(PicturesContext);
-
-  const handleCheckBox = (id: string) => {
-    const selection = pictures.find((picture) => picture.id === id);
-
-    if (selection) {
-      selection.selected = !selection.selected;
-      const updatedPictures = pictures.map((picture) =>
-        picture.id === selection.id
-          ? {
-              ...picture,
-              selected: selection?.selected,
-            }
-          : picture
-      );
-      setPictures(updatedPictures);
-      addDeleteFromCheckBox(selection, id);
-    }
-  };
-
-  const addDeleteFromCheckBox = (selection: PictureInfoVm, id: string) => {
-    const updateWithDeletedPicture = selectedPictures.filter(
-      (picture) => picture !== selection.id
-    );
-
-    if (selection.selected === true)
-      setSelectedPictures([...selectedPictures, id]);
-    else {
-      setSelectedPictures(updateWithDeletedPicture);
-    }
-  };
-
-  useEffect(() => {
-    getPictures().then((apiPictures) => {
-      const mappedPictures = mapPictureCollectionFromApiToVm(
-        apiPictures,
-        selectedPictures
-      );
-      setPictures(mappedPictures);
-    });
-  }, [selectedPictures, setPictures]);
-
   return (
-    <LandscapeCategoryComponent
-      pictures={pictures}
-      handleCheckBox={handleCheckBox}
+    <ImageCategoryContainer
+      getPictures={getPictures}
+      mapPictureCollectionFromApiToVm={mapPictureCollectionFromApiToVm}
     />
   );
 };
