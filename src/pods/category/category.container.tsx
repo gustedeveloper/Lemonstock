4c02212acc62d2d9c5684a9b1fb120a0b6e966b3
Guste Gaubaite <219.guste@gmail.com>
2025-09-08 11:42:59 +0200

    fix: resolve image disappearing issue when selecting checkbox

--- src/pods/category/category.container.tsx
+++ src/pods/category/category.container.tsx
@@ -1,11 +1,9 @@
-import { FC, useContext } from "react";
+import { FC } from "react";
 import { ImageCategoryContainer } from "../image-category";
-import { PicturesContext } from "../../core/context/pictures-context";
 import { getPicturesFromPexels } from "../../core/api/pexels.api"; // ajusta path
 import { useParams } from "react-router-dom";
 
 export const CategoryContainer: FC = () => {
-  const { selectedPictures } = useContext(PicturesContext);
   const { categoryName } = useParams<{ categoryName: string }>();
 
   const apiQuery = categoryName?.replace(/-/g, " ") || "";
@@ -13,7 +11,7 @@ export const CategoryContainer: FC = () => {
   return (
     <ImageCategoryContainer
       getPictures={(page, perPage) =>
-        getPicturesFromPexels(apiQuery, selectedPictures, perPage, page)
+        getPicturesFromPexels(apiQuery, [], perPage, page)
       }
     />
   );
